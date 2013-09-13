07a391f4454462f141fc76d8177b1cf0defeb3e9
Stephen  Mather <mather.stephen@gmail.com>
2013-09-13 13:02:44 -0400

    Further refinement of geomhashstring.sql
    still needs modified for taking arguments... .

--- geomhashstring.sql
+++ geomhashstring.sql
@@ -1,12 +1,11 @@
 CREATE OR REPLACE FUNCTION geohashstring (geometry) RETURNS text AS $$
  
-WITH royalview2 AS (
+WITH unionT AS (
 	SELECT ST_Transform(ST_Union(geom), 4326) AS geom FROM royalview)
-,royalview2_hand AS(
-	SELECT ST_Transform(ST_Union(geom), 4326) AS geom FROM royalview_hand)
 ,geohash AS (
-	SELECT ST_GeoHash((ST_DumpPoints(geom)).geom) AS hash FROM royalview2)
+	SELECT ST_GeoHash((ST_DumpPoints(geom)).geom) AS hash FROM unionT )
 
 SELECT STRING_AGG(hash, ',') FROM geohash;
  
 $$ LANGUAGE SQL;
+
