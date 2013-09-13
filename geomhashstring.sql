cbb7d92f62bf8faeb715ff0555413c9131b2f983
Stephen  Mather <mather.stephen@gmail.com>
2013-09-13 12:50:07 -0400

    Starting to work on hashing string
    This is the first step toward getting strings to compare:
    Reprojects to proj 4326, dumps to points, geohashes those points, then
    aggregates them into a single string.

---
+++ geomhashstring.sql
@@ -0,0 +1,8 @@
+WITH royalview2 AS (
+	SELECT ST_Transform(ST_Union(geom), 4326) AS geom FROM royalview)
+,royalview2_hand AS(
+	SELECT ST_Transform(ST_Union(geom), 4326) AS geom FROM royalview_hand)
+,geohash AS (
+	SELECT ST_GeoHash((ST_DumpPoints(geom)).geom) AS hash FROM royalview2)
+
+SELECT STRING_AGG(hash, ',') FROM geohash;
