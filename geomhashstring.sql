4908bf4c58992b973854a8ea8475110b57689acd
Stephen  Mather <mather.stephen@gmail.com>
2013-09-13 12:59:50 -0400

    Turning geomhashstring.sql into a function

--- geomhashstring.sql
+++ geomhashstring.sql
@@ -1,3 +1,5 @@
+CREATE OR REPLACE FUNCTION geohashstring (geometry) RETURNS text AS $$
+ 
 WITH royalview2 AS (
 	SELECT ST_Transform(ST_Union(geom), 4326) AS geom FROM royalview)
 ,royalview2_hand AS(
@@ -6,3 +8,5 @@ WITH royalview2 AS (
 	SELECT ST_GeoHash((ST_DumpPoints(geom)).geom) AS hash FROM royalview2)
 
 SELECT STRING_AGG(hash, ',') FROM geohash;
+ 
+$$ LANGUAGE SQL;
